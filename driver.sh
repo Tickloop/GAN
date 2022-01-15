d70db6e96edde03775f73e07f10acfd4a1685fe8
Tickloop <pulkitarya2000@gmail.com>
2022-01-15 16:55:24 -0500

    Added argument parser and generated results from 100 epochs for 0-9

---
+++ driver.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python gan.py --digit 0
+python gan.py --digit 1
+python gan.py --digit 2
+python gan.py --digit 3
+python gan.py --digit 4
+python gan.py --digit 5
+python gan.py --digit 6
+python gan.py --digit 7
+python gan.py --digit 8
+python gan.py --digit 9
--- gan.py
+++ gan.py
@@ -5,11 +5,18 @@ from tqdm import tqdm # this is used for progress bars
 import numpy as np
 import torch.nn as nn
 import torch
+import argparse
 
-img_size = 28
-latent_dim = 64
-batch_size = 100
-MAX_EPOCH = 100
+parser = argparse.ArgumentParser()
+parser.add_argument("--epochs", default=100, type=int, help="Number of epochs to run the network for")
+parser.add_argument("--latent_dim", default=64, type=int, help="The size of the noise vector to pass in the Generator")
+parser.add_argument("--batch_size", default=100, type=int, help="Size of mini batch that is fed into one step of network training")
+parser.add_argument("--img_size", default=28, type=int, help="Size of generated images")
+parser.add_argument("--digit", default=0, type=int, help="MNIST digit to be generated")
+options = parser.parse_args()
+print("Selected arguments to run: ")
+print(options)
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("Device: ", device)
 
@@ -17,25 +24,25 @@ class Generator(nn.Module):
     """
         The Generator class is responsible for generating new data which must pass through the discriminator.
         To generate new data, random noise input is required.
-        The output size in 512 x 512 or N x 512 x 512 for batch processing.
+        The output size in img_size x img_size or N x img_size x img_size for batch processing.
     """
     def __init__(self):
         super(Generator, self).__init__()
 
         self.model = nn.Sequential(
-            nn.Linear(latent_dim, 128),
+            nn.Linear(options.latent_dim, 128),
             nn.Sigmoid(),
             nn.Linear(128, 256),
             nn.Sigmoid(),
             nn.Linear(256, 512),
             nn.Sigmoid(),
-            nn.Linear(512, img_size * img_size),
+            nn.Linear(512, options.img_size * options.img_size),
             nn.Sigmoid()
         )
     
     def forward(self, x):
         img = self.model(x)
-        img = torch.reshape(img, (batch_size, img_size, img_size))
+        img = torch.reshape(img, (options.batch_size, options.img_size, options.img_size))
         return img
 
 class Discriminator(nn.Module):
@@ -48,7 +55,7 @@ class Discriminator(nn.Module):
 
         self.model = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(img_size * img_size, 512),
+            nn.Linear(options.img_size * options.img_size, 512),
             nn.Sigmoid(),
             nn.Linear(512, 256),
             nn.Sigmoid(),
@@ -63,14 +70,10 @@ class Discriminator(nn.Module):
         return is_valid
 
 def dataLoader(images):
-    """
-        Function is responsible for reading image files from data/mnist/trainingSet/trainingSet/0 directory
-        and resizing them to 512 x 512 so they can be fed into our GAN for training.
-    """
-    for path, dirs, filenames in os.walk('data/mnist/trainingSet/trainingSet/0'):
+    """ Function is responsible for reading image files from data/mnist/trainingSet/trainingSet/{mnist_number} directory """
+    for path, dirs, filenames in os.walk(f"data/mnist/trainingSet/trainingSet/{options.digit}"):
         for filename in tqdm(filenames):
             img = cv2.imread(f"{path}/{filename}", 0)
-            img = cv2.resize(img, (img_size, img_size))
             images.append(img)
 
 def train(generator, discriminator, images):
@@ -84,19 +87,19 @@ def train(generator, discriminator, images):
     # total number of real data points that we have
     N = images.shape[0]
 
-    for epoch in range(MAX_EPOCH):
-        z_mini_batch = torch.Tensor(np.random.normal(0, 1, size=(batch_size, latent_dim))) # size (batch_size x laten_dim)
+    for epoch in range(options.epochs):
+        z_mini_batch = torch.Tensor(np.random.normal(0, 1, size=(options.batch_size, options.latent_dim))) # size (batch_size x laten_dim)
         z_mini_batch = z_mini_batch.to(device)
         
-        random_choice = np.random.choice(np.arange(N), size=batch_size) # size (batch_size x 1)
+        random_choice = np.random.choice(np.arange(N), size=options.batch_size) # size (batch_size x 1)
         real_images = torch.Tensor(np.array([images[choice] for choice in random_choice])) # size (batch_size x img_size x img_size x channels)
         real_images = real_images.to(device)
               
 
         # labels for real and fake data
-        real_data_labels = torch.Tensor(np.ones(shape=(batch_size, 1))) # size (batch_size x 1)
+        real_data_labels = torch.Tensor(np.ones(shape=(options.batch_size, 1))) # size (batch_size x 1)
         real_data_labels = real_data_labels.to(device)
-        fake_data_labels = torch.Tensor(np.zeros(shape=(batch_size, 1))) # size (batch_size x 1)
+        fake_data_labels = torch.Tensor(np.zeros(shape=(options.batch_size, 1))) # size (batch_size x 1)
         fake_data_labels = fake_data_labels.to(device)
 
         # train the discriminator
@@ -113,7 +116,7 @@ def train(generator, discriminator, images):
         # train the generator
         generator.zero_grad()
 
-        z_mini_batch = torch.Tensor(np.random.normal(0, 1, size=(batch_size, latent_dim))) # size (batch_size x laten_dim)
+        z_mini_batch = torch.Tensor(np.random.normal(0, 1, size=(options.batch_size, options.latent_dim))) # size (batch_size x laten_dim)
         z_mini_batch = z_mini_batch.to(device)
 
         fake_images = generator(z_mini_batch) # size (batch_size x img_size x img_size x channels)
@@ -125,10 +128,10 @@ def train(generator, discriminator, images):
         generator_loss.backward()
         generater_optimizer.step()
 
-        print(f"Epoch: {epoch + 1} / {MAX_EPOCH}, Generator Loss: {generator_loss} , Discriminator Loss: {discriminator_loss}")
+        print(f"Epoch: {epoch + 1} / {options.epochs}, Generator Loss: {generator_loss} , Discriminator Loss: {discriminator_loss}")
 
-        if epoch % 5 == 0:
-            cv2.imwrite(f"./samples/0/sample_epoch_{epoch}.jpg", fake_images[0].cpu().detach().numpy() * 255)
+        cv2.imwrite(f"./samples/{options.digit}/sample_epoch_{epoch}.jpg", fake_images[0].cpu().detach().numpy() * 255)
+        
 
 def main():
     images = []
@@ -144,6 +147,13 @@ def main():
     # let's go faster
     generator.to(device)
     discriminator.to(device)
+
+    # ensure that there is a place to store generated samples
+    if not os.path.isdir("samples"):
+        os.mkdir("samples")
+    
+    if not os.path.isdir(f"samples/{options.digit}"):
+        os.mkdir(f"samples/{options.digit}")
 
     train(generator, discriminator, images)
 
